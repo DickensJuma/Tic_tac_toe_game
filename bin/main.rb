21ae856ba8cc81d1300cf85d1e0e7cbe11d7e62d
DickensJuma <dickensjuma13@gmail.com>
2020-06-25 18:29:44 +0300

    removed comments on main.rb

--- bin/main.rb
+++ bin/main.rb
@@ -1,16 +1,16 @@
 #!/usr/bin/env ruby
 # frozen_string_literal: true
 
-# This is the welcome page for the players
+
 puts ' Welcome to Tic Tac Toe'
 puts 'Please player one give us your name:'
 player1 = gets.chomp
-# The name of the first player is stored here
+
 puts 'Please player two give us your name:'
 player2 = gets.chomp
 puts "Please #{player1} choose between x or o"
 selection = gets.chomp.to_s
-# The name of the second player is stored here
+
 if selection == 'x'
   puts "#{player1}, you are #{selection} for this game"
   puts "#{player2}, you are o for this game"
@@ -18,7 +18,7 @@ elsif selection == 'o'
   puts "#{player1}, you are #{selection} for this game"
   puts "#{player2}, you are x for this game"
 end
-# used selection to save whatever the value the first player chooses
+
 def board
   puts
   puts '1|2|3'
@@ -27,13 +27,12 @@ def board
   puts  '-----'
   puts  '7|8|9'
 end
-# method displaying the game interface, by asking the first player to choose a
-# move and the move value is saved as input and input2 for both players
+
 board
 puts "#{player1} its your turn to make a move"
 puts 'Choose move between 0 to 9'
 input = gets.chomp.to_i
-# we forced the two players to make sure they enter a valid value of 0-9
+
 until input.between?(0, 9)
   puts ' you did not choose correct move, choose again'
   input = gets.chomp.to_i
@@ -48,8 +47,7 @@ until input2.between?(0, 9)
   puts ' you did not choose correct move, ch oose again'
   input2 = gets.chomp.to_i
 end
-# game logic is showed below, whatever input from the players is cehcked
-# whether it's a winning, draw, or lost move.
+
 count = 0
 loop do
   until game_over
@@ -65,45 +63,4 @@ loop do
   end
 end
 
-# def move(board_arr, arg,current_ player)
-#   board[arg]=current_player
-#
-# end
-#
-# def count
-#   result=0
-#   board_arr.each do |n|
-#     if n=="x" || n=="o"
-#       result = result+1
-#     end
-#   end
-#   result
-# end
-# def current_player
-# if count(board_arr) % 2==0
-#   puts "x"
-# elsif
-#   puts "o"
-# end
-# end
-#
-# end
-# def valid(arg)
-#   if arg.between(1..9)
-#     return true
-#   else
-#     return false
-# end
-# def over(board_arr)
-#   if full(board_arr) || won(board_arr) || draw(board_arr)
-#     return true
-#     puts "game over"
-#   end
-#
-#  end
-#
-#   def board_full(board_arr)
-#     board_arr.all? do |n|
-#       n=="x" || n=="o"
-#   end
-#   end
+

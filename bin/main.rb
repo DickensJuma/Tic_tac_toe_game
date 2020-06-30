a5cca5987e0c8707550457d288de0a159af51882
haraphat01 <haraphat@gmail.com>
2020-06-30 15:25:38 +0100

    adding on the logic

--- bin/main.rb
+++ bin/main.rb
@@ -5,7 +5,6 @@ require_relative('../lib/player.rb')
 require_relative('../lib/logic.rb')
 
 
-
 new_game =Board.new
  
 puts ' Welcome to Tic Tac Toe'            
@@ -13,24 +12,31 @@ puts 'Please player one give us your name:'
 player1 = gets.chomp                                                                  
 puts 'Please player two give us your name:'
 player2 = gets.chomp                      
-puts "Please #{player1} choose between X or
-O"  
+puts "Please #{player1} choose between X or O"  
 selection1=gets.chomp.upcase
-puts "#{player2} it's your turn to make a move"
-puts 'Choose move between 1 to 9'
-input2 = gets.chomp.to_i
-
-until input2.between?(1, 9)
-  puts ' you did not choose correct move, choose again'
-  input2 = gets.chomp.to_i
-until selection1 == "X" || selection1 == "O"
-puts "Please #{player1} choose between X or O"
-
 selection2 = selection1 == 'X' ? 'O' : 'X'
-puts "#{player1}:#{selection1}"
-puts "#{player2}: #{selection
+puts "#{player1} You are :#{selection1}"
+puts "#{player2}: You are: #{selection2}"
 puts new_game.display_board
 
+puts "Hey #{player1} it's your turn to make a move"
+puts 'Choose move between 1 to 9'
+input1 = gets.chomp.to_i
+until input1.between?(1, 9)
+ puts ' you did not choose the correct move, choose again'
+input1 = gets.chomp.to_i
+
+puts "Hey #{player2} it's your turn to make a move"
+puts 'Choose move between 1 to 9'
+  input2 = gets.chomp.to_i
+  until input2.between?(1, 9)
+    puts ' you did not choose correct move, choose again'
+  input2 = gets.chomp.to_i
+
+
+
+until selection1 == "X" || selection1 == "O"
+puts "Please #{player1} choose between X or O"
 puts "#{player1} its your turn to make a move"
 puts 'Choose move between 1 to 9'
 move = gets.chomp.to_i
@@ -44,5 +50,7 @@ until valid_move?(input)
   new_game.move(current_player,index,board)
 
   end
+end
+end
 end
 end
--- lib/board.rb
+++ lib/board.rb
@@ -6,11 +6,11 @@ class Board
   end
 
   def display_board
-  " #{@board[0]} | #{@board[1]} | #{@board[2]}"
-   "--------"
-  " #{@board[3]} | #{@board[4]} | #{@board[5]}"
-  "--------"
-  " #{@board[6]} | #{@board[7]} | #{@board[8]}"
+  "   #{@board[0]} | #{@board[1]} | #{@board[2]}
+   ----------
+   #{@board[3]} | #{@board[4]} | #{@board[5]}
+  ----------
+   #{@board[6]} | #{@board[7]} | #{@board[8]}"
   end
 
   def position_taken?(board, index)                 

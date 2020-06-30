08090c6cf1fc22e885d560dab072684014ec0b01
haraphat01 <haraphat@gmail.com>
2020-06-30 10:17:55 +0100

    adding on the logic

--- bin/main.rb
+++ bin/main.rb
@@ -21,14 +21,14 @@ puts 'Choose move between 1 to 9'
 input2 = gets.chomp.to_i
 
 until input2.between?(1, 9)
-  puts ' you did not choose correct move, ch oose again'
+  puts ' you did not choose correct move, choose again'
   input2 = gets.chomp.to_i
 until selection1 == "X" || selection1 == "O"
 puts "Please #{player1} choose between X or O"
 
 selection2 = selection1 == 'X' ? 'O' : 'X'
 puts "#{player1}:#{selection1}"
-puts "#{player2}: #{selection2}"
+puts "#{player2}: #{selection
 puts new_game.display_board
 
 puts "#{player1} its your turn to make a move"
@@ -37,14 +37,12 @@ move = gets.chomp.to_i
 
   
 new_game.move(current_player=player1,board,index)
-
 until valid_move?(input)
   puts ' you did not choose correct move, choose again'
   input = gets.chomp.to_i
      if input == position_taken
   new_game.move(current_player,index,board)
 
-     end
-end
+  end
 end
 end
